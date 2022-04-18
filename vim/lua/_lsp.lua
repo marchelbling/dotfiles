8f05a45a9bb5e8f60870658cc3a1f981e8864818
Marc Helbling <marc.d.helbling@gmail.com>
2022-04-18 21:41:21 +0200

    use stylua
    Enable lua formatter and save all existing lua configuration files.

--- vim/lua/_lsp.lua
+++ vim/lua/_lsp.lua
@@ -1,124 +1,124 @@
 local has_cmp, cmp = pcall(require, "cmp")
 if not has_cmp then
-    return
+	return
 end
 
 local has_lspconfig, lspconfig = pcall(require, "lspconfig")
 if not has_lspconfig then
-    return
+	return
 end
 
 local has_lsp_signature, lsp_signature = pcall(require, "lsp_signature")
 if not has_lsp_signature then
-    return
+	return
 end
 
 -- show function signature
 lsp_signature.setup({
-    bind = true,
-    hint_enable = false,
-    max_width = 120,
-    handler_opts = { border = "none" },
+	bind = true,
+	hint_enable = false,
+	max_width = 120,
+	handler_opts = { border = "none" },
 })
 
 -- helper function for mappings
 local m = function(mode, key, result)
-    vim.api.nvim_buf_set_keymap(0, mode, key, "<cmd> " .. result .. "<cr>", {
-        noremap = true,
-        silent = true,
-    })
+	vim.api.nvim_buf_set_keymap(0, mode, key, "<cmd> " .. result .. "<cr>", {
+		noremap = true,
+		silent = true,
+	})
 end
 
 -- function to attach completion when setting up lsp
 local on_attach = function(client)
-    -- Mappings.
-    m("n", "ga", "lua vim.lsp.buf.code_action()")
-    m("n", "gD", "lua vim.lsp.buf.declaration()")
-    m("n", "gd", "lua vim.lsp.buf.definition()")
-    m("n", "ge", "lua vim.lsp.diagnostic.goto_next()")
-    m("n", "gE", "lua vim.lsp.diagnostic.goto_prev()")
-    m("n", "gi", "lua vim.lsp.buf.implementation()")
-    m("n", "gr", "lua vim.lsp.buf.references()")
-    m("n", "H", "lua vim.lsp.buf.hover()")
-    -- m("n", "<space>rn", "lua vim.lsp.buf.rename()")
-    m("n", "gl", "lua vim.lsp.diagnostic.show_line_diagnostics()")
-    -- m("n", "<space>f", "lua vim.lsp.buf.formatting()")
+	-- Mappings.
+	m("n", "ga", "lua vim.lsp.buf.code_action()")
+	m("n", "gD", "lua vim.lsp.buf.declaration()")
+	m("n", "gd", "lua vim.lsp.buf.definition()")
+	m("n", "ge", "lua vim.lsp.diagnostic.goto_next()")
+	m("n", "gE", "lua vim.lsp.diagnostic.goto_prev()")
+	m("n", "gi", "lua vim.lsp.buf.implementation()")
+	m("n", "gr", "lua vim.lsp.buf.references()")
+	m("n", "H", "lua vim.lsp.buf.hover()")
+	-- m("n", "<space>rn", "lua vim.lsp.buf.rename()")
+	m("n", "gl", "lua vim.lsp.diagnostic.show_line_diagnostics()")
+	-- m("n", "<space>f", "lua vim.lsp.buf.formatting()")
 
-    local has_illuminate, illuminate = pcall(require, "illuminate")
-    if  has_illuminate then
-        illuminate.on_attach(client)
-    end
+	local has_illuminate, illuminate = pcall(require, "illuminate")
+	if has_illuminate then
+		illuminate.on_attach(client)
+	end
 
-    -- delegate forumatting to null-ls
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+	-- delegate forumatting to null-ls
+	client.resolved_capabilities.document_formatting = false
+	client.resolved_capabilities.document_range_formatting = false
 end
 
 -- diagnostics
 vim.diagnostic.config({
-    virtual_text = false,
-    underline = true,
-    float = { source = "always" },
-    severity_sort = true,
-    signs = true,
-    update_in_insert = false,
+	virtual_text = false,
+	underline = true,
+	float = { source = "always" },
+	severity_sort = true,
+	signs = true,
+	update_in_insert = false,
 })
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = {
-    pyright = {},
-    gopls = {
-        cmd = { "gopls", "serve" },
-        settings = {
-            gopls = {
-                codelenses = {
-                    test = true, -- Runs go test for a specific set of test or benchmark functions
-                    tidy = true, -- Runs go mod tidy for a module
-                    vendor = true, -- Runs go mod vendor for a module
-                },
-                gofumpt = false, -- A stricter gofmt
-                usePlaceholders = true, -- enables placeholders for function parameters or struct fields in completion responses
-            },
-        },
-    },
-    solargraph = {},
-    terraformls = {},
-    rust_analyzer = {
-        assist = {
-            importGranularity = "module",
-            importPrefix = "by_self",
-        },
-        cargo = {
-            loadOutDirsFromCheck = true
-        },
-        procMacro = {
-            enable = true
-        },
-        checkOnSave = {
-            command = "clippy"
-        },
-    }
+	pyright = {},
+	gopls = {
+		cmd = { "gopls", "serve" },
+		settings = {
+			gopls = {
+				codelenses = {
+					test = true, -- Runs go test for a specific set of test or benchmark functions
+					tidy = true, -- Runs go mod tidy for a module
+					vendor = true, -- Runs go mod vendor for a module
+				},
+				gofumpt = false, -- A stricter gofmt
+				usePlaceholders = true, -- enables placeholders for function parameters or struct fields in completion responses
+			},
+		},
+	},
+	solargraph = {},
+	terraformls = {},
+	rust_analyzer = {
+		assist = {
+			importGranularity = "module",
+			importPrefix = "by_self",
+		},
+		cargo = {
+			loadOutDirsFromCheck = true,
+		},
+		procMacro = {
+			enable = true,
+		},
+		checkOnSave = {
+			command = "clippy",
+		},
+	},
 }
 for server, config in pairs(servers) do
-  lspconfig[server].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = config.settings or {}
-  }
+	lspconfig[server].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		settings = config.settings or {},
+	})
 end
 
 -- diagnostics
 vim.diagnostic.config({
-    virtual_text = false,
-    underline = true,
-    float = {
-        source = "always",
-    },
-    severity_sort = true,
-    --[[ virtual_text = {
+	virtual_text = false,
+	underline = true,
+	float = {
+		source = "always",
+	},
+	severity_sort = true,
+	--[[ virtual_text = {
       prefix = "»",
       spacing = 4,
     }, ]]
-    signs = true,
-    update_in_insert = false,
+	signs = true,
+	update_in_insert = false,
 })
--- vim/lua/_null-ls.lua
+++ vim/lua/_null-ls.lua
@@ -1,31 +1,30 @@
 local has_null_ls, null_ls = pcall(require, "null-ls")
 if not has_null_ls then
-    return
+	return
 end
-
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
-    formatting.gofumpt,
-    formatting.goimports,
-    diagnostics.golangci_lint,
+	formatting.gofumpt,
+	formatting.goimports,
+	diagnostics.golangci_lint,
+	formatting.stylua,
 }
 
 null_ls.setup({
-    -- configuration:
-    sources = sources,
-    -- format on save
-    on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-            vim.cmd([[
+	-- configuration:
+	sources = sources,
+	-- format on save
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
                 autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
             augroup END
             ]])
-        end
-    end,
+		end
+	end,
 })
-
