dbffdfe17b64cab07f2469ff5ca1a1b58d97939a
Marc Helbling <marc.d.helbling@gmail.com>
2022-08-09 21:26:39 +0200

    run stylua

--- vim/lua/_null-ls.lua
+++ vim/lua/_null-ls.lua
@@ -8,32 +8,31 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-
 null_ls.setup({
 	-- configuration:
 	sources = {
-        -- lua
-        formatting.stylua,
-        -- golang
-        formatting.gofumpt,
-        formatting.goimports,
-        diagnostics.golangci_lint,
-        -- js
-        formatting.prettier,
-        code_actions.eslint,
-    },
+		-- lua
+		formatting.stylua,
+		-- golang
+		formatting.gofumpt,
+		formatting.goimports,
+		diagnostics.golangci_lint,
+		-- js/html
+		formatting.prettier,
+		code_actions.eslint,
+	},
 	-- format on save
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
