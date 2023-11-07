7c1fa1b1dbebe0549a059274ad572d90ede20fdc
Marc Helbling <marc.d.helbling@gmail.com>
2023-11-07 18:36:16 +0100

    add toggable terminal in nvim

---
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+	"https://github.com/akinsho/toggleterm.nvim",
+	-- keys = {
+	-- 	{
+	-- 		"<c-t>",
+	-- 		"<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+	-- 	},
+	-- },
+	config = {
+		open_mapping = "<C-t>",
+	},
+}
