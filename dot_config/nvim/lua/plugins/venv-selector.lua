a538a5cee1ff3d941520442f8a95794ff9381033
Marc Helbling <marc.d.helbling@gmail.com>
2024-01-18 22:57:10 +0100

    improve python setup

---
+++ dot_config/nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,16 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+	opts = {
+		name = { ".venv" },
+		parents = 3,
+		-- auto_refresh = false
+	},
+	event = "VeryLazy",
+	keys = {
+		-- Keymap to open VenvSelector to pick a venv.
+		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+	},
+}
