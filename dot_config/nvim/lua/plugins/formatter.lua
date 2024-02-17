e3d315e53ed39f8f0384e66f36898d6e52dc260d
Marc Helbling <marc.d.helbling@gmail.com>
2024-02-17 22:58:03 +0100

    setup formatter for markdown

--- dot_config/nvim/lua/plugins/formatter.lua
+++ dot_config/nvim/lua/plugins/formatter.lua
@@ -57,6 +57,22 @@ return {
 						}
 					end,
 				},
+				markdown = {
+					function()
+						return {
+							exe = "prettier",
+							stdin = true,
+							args = {
+								"--print-width",
+								90,
+								"--prose-wrap",
+								"always",
+								"--stdin-filepath",
+								util.escape_path(util.get_current_buffer_file_path()),
+							},
+						}
+					end,
+				},
 			},
 		})
 	end,
