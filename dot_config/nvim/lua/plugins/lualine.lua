64782c6aed1de64ced818bcc1047838f758cbd45
Marc Helbling <marc.d.helbling@gmail.com>
2024-11-03 16:03:04 +0100

    remove noice plugin

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,20 +5,5 @@ return {
 			icons_enabled = true,
 			theme = "nord",
 		},
-		sections = {
-			lualine_x = {
-				{
-					require("noice").api.statusline.mode.get,
-					cond = require("noice").api.statusline.mode.has,
-					color = { fg = "#ff9e64" },
-				},
-				{
-					require("noice").api.status.command.get,
-					cond = require("noice").api.status.command.has,
-					color = { fg = "#ff9e64" },
-				},
-			},
-			lualine_y = { "searchcount" },
-		},
 	},
 }
