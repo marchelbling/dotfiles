6261543fcc900b39f29078a2e3e8346b02c737a2
Marc Helbling <marc.d.helbling@gmail.com>
2026-01-02 18:20:05 +0100

    update nvim config

--- dot_config/nvim/lua/plugins/venv-selector.lua
+++ dot_config/nvim/lua/plugins/venv-selector.lua
@@ -3,12 +3,12 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 		"mfussenegger/nvim-dap",
-		"mfussenegger/nvim-dap-python", --optional
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		"mfussenegger/nvim-dap-python",
+		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
 	lazy = false,
-	branch = "regexp",
-	config = function()
-		require("venv-selector").setup()
-	end,
+	opts = {
+		search = {},
+		options = {},
+	},
 }
--- dot_config/nvim/lua/plugins/vim-matchup.lua
+++ dot_config/nvim/lua/plugins/vim-matchup.lua
@@ -1,16 +1,8 @@
 return {
-	"https://github.com/andymass/vim-matchup",
-	lazy = false,
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			matchup = {
-				enable = true, -- mandatory, false will disable the whole extension
-				disable = {}, -- optional, list of language that will be disabled
-				enable_quotes = true,
-				disable_virtual_text = false,
-				include_match_words = true,
-			},
-		})
+	"andymass/vim-matchup",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	init = function()
+		vim.g.matchup_matchparen_enabled = 1
+		vim.g.matchup_matchparen_offscreen = { method = "popup" }
 	end,
 }
